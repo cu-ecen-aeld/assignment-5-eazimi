a6f7941e9bc05bc14d0625a945b93be4fa1ce69b
Ehsan Azimi <ehsan.azimi@kaco-newenergy.de>
2023-07-07 13:39:19 +0200

    reinstate PATH in assignment 3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'aedef85a5323f8d0a138b9702b68db25444d0f2a'
+AESD_ASSIGNMENTS_VERSION = 'e2685b5cf6ca36a6465442737c5b09db49bdaf31'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
