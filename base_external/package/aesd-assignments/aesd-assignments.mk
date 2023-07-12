55969783d7b895ccbb14bb676fcf609bd0a4ff97
Ehsan Azimi <ehs.azimi22@gmail.com>
2023-07-12 09:52:07 +0200

    write in the output file before calling exit in assignment 3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5e445e306e8e4bf9bcc716a117ed4011174a01f2'
+AESD_ASSIGNMENTS_VERSION = 'df0da0639f5fdf4af2b57603ee950902bbd78b79'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
