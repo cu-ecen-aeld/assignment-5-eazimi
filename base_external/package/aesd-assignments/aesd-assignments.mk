ff34775d619fadbda684b2b88a0d42881aa761fc
Ehsan Azimi <ehs.azimi22@gmail.com>
2023-08-21 13:58:26 +0200

    use latest commit of assignment 3, assignment 5 completed

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,13 +6,13 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '729da557a360e7773660ecd6b829c0ae38591827'
+AESD_ASSIGNMENTS_VERSION = '9686b88e53f918d19a559e5c71710e3c8a4fb103'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-# AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-eazimi.git'
-AESD_ASSIGNMENTS_SITE = ''
-AESD_ASSIGNMENTS_OVERRIDE_SRCDIR = "${HOME}/cu-ecen-aeld/assignments-3-and-later-eazimi"
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-eazimi.git'
+# AESD_ASSIGNMENTS_SITE = ''
+# AESD_ASSIGNMENTS_OVERRIDE_SRCDIR = "${HOME}/cu-ecen-aeld/assignments-3-and-later-eazimi"
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
